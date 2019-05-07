965edebb08a494f6f52e7a8783537505e2672c72
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2019-05-07 15:28:14 +0700

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,15 @@ module.exports = {
   title: "Hello World Collection",
   description: "Collection of Hello World in Any Language",
   base: "/hello-world/",
-  dest: "public"
+  dest: "public",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      {
+        text: "Github",
+        link: "https://github.com/berviantoleo/hello-world"
+      }
+    ],
+    sidebar: ["/"]
+  }
 }
